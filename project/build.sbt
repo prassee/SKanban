804cc87642ede0587482615ca5663fd910a5b58a
Prasanna Kumar Sathyanarayanan <prasanna.sathyanarayanan@csscorp.com>
2014-01-20 11:54:02 +0530

    restore the position of cards in the lanes

--- project/build.sbt
+++ project/build.sbt
@@ -3,6 +3,4 @@ resolvers += Resolver.url("scala-js-releases",
 
 addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.2")
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
-
 addSbtPlugin("org.netbeans.nbsbt" % "nbsbt-plugin" % "1.1.0")
--- src/main/scala/skanban/SKanban.scala
+++ src/main/scala/skanban/SKanban.scala
@@ -4,79 +4,99 @@ import org.scalajs.dom
 import org.scalajs.dom._
 
 object SKanban {
-  val d = dom.document
-  var srcLane = ""
-  var destLane = ""
-  val cols = List("Todo", "Doing", "Done")
-  val x = window.localStorage
+    val d = dom.document
+    var srcLane = ""
+    var destLane = ""
+    val cols = List("Todo", "Doing", "Done")
+    val localstore = window.localStorage
 
-  def main(): Unit = {
-    cols.foreach(col => {
-      val outerCol = d.createElement("div")
-      outerCol.className = "col-md-4"
-      outerCol.innerHTML = s"<h3><span class='label label-default'>${col}</span></h3>"
-      val innerCol = d.createElement("div")
-      innerCol.id = col
-      innerCol.className = "breadcrumb lane"
-      innerCol.ondrop = (de: DragEvent) => {
-        destLane = (de.target.asInstanceOf[HTMLDivElement]).id
-        ColumnEventHandlers.onDropFn(de, srcLane, destLane)
-      }
-      innerCol.ondragover = (de: DragEvent) => ColumnEventHandlers.ondragover(de)
-      // innerCol.innerHTML = (x(col)).toString
-      outerCol.appendChild(innerCol)
-      d.getElementById("board").appendChild(outerCol)
-    })
-  }
-
-  def createCard() {
-    val x = ((d.getElementById("cname").asInstanceOf[HTMLInputElement]).value,
-      (d.getElementById("cdesc").asInstanceOf[HTMLInputElement]).value)
-    val card = d.createElement("div")
-    card.className = "breadcrumb"
-    card.draggable = true
-    card.id = x._1
-    card.ondblclick = (e: MouseEvent) => e.target
-    card.ondragstart = (e: DragEvent) => {
-      val par = (e.target.asInstanceOf[HTMLDivElement]).id
-      srcLane = document.getElementById(par).parentElement.id
-      ColumnEventHandlers.dragFn(e)
+    /**
+     * *
+     * this method plots all lanes in the board
+     * and called in the startup of the app
+     */
+    def main(): Unit = {
+        cols.foreach(col => {
+            val outerCol = d.createElement("div")
+            outerCol.className = "col-md-4"
+            outerCol.innerHTML = s"<h3><span class='label label-default'>${col}</span></h3>"
+            val innerCol = d.createElement("div")
+            innerCol.id = col
+            innerCol.className = "breadcrumb lane"
+            innerCol.ondrop = (de: DragEvent) => {
+                destLane = (de.target.asInstanceOf[HTMLDivElement]).id
+                ColumnEventHandlers.onDropFn(de, srcLane, destLane)
+            }
+            innerCol.ondragover = (de: DragEvent) => ColumnEventHandlers.ondragover(de)
+            // innerCol.innerHTML = (x(col)).toString
+            outerCol.appendChild(innerCol)
+            d.getElementById("board").appendChild(outerCol)
+        })
+    
+        cols.foreach(col => restoreBoards(col))
     }
-    val content = d.createElement("div")
-    content.className = "caption"
-    content.innerHTML = s"<p>${x._2}</p>"
-    card.appendChild(content)
-    d.getElementById("Todo").appendChild(card)
-  }
 
-  def clearBoard {
-    x.clear()
-    cols.foreach{ col =>
-      d.getElementById(col).innerHTML = ""
+    def restoreBoards(lane: String) {
+        val cards = localstore(lane).toString.split("\n")
+        cards.foreach { card =>
+            val cardDetail = card.split(":")
+            addCardToBoard(cardDetail(0), cardDetail(1), lane)
+            console.log(s"${lane} -> ${cardDetail(0)} , ${cardDetail(1)}")
+        }
     }
-  }
+
+    def addCardToBoard(title: String, desc: String, lane: String) {
+        val card = d.createElement("div")
+        card.className = "breadcrumb"
+        card.draggable = true
+        card.id = title
+        card.innerHTML = title
+        card.ondblclick = (e: MouseEvent) => e.target
+        card.ondragstart = (e: DragEvent) => {
+            val par = (e.target.asInstanceOf[HTMLDivElement]).id
+            srcLane = document.getElementById(par).parentElement.id
+            ColumnEventHandlers.dragFn(e)
+        }
+        val content = d.createElement("div")
+        content.className = "caption hid"
+        content.innerHTML = s"<p>${title}</p>"
+        card.appendChild(content)
+        d.getElementById(lane).appendChild(card)
+    }
+
+    def createCard() {
+        val x = ((d.getElementById("cname").asInstanceOf[HTMLInputElement]).value,
+            (d.getElementById("cdesc").asInstanceOf[HTMLInputElement]).value)
+        addCardToBoard(x._1, x._2, "Todo")
+    }
+
+    def clearBoard {
+        localstore.clear()
+        cols.foreach { col =>
+            d.getElementById(col).innerHTML = ""
+        }
+    }
 }
 
 object ColumnEventHandlers {
 
-  def dragFn(de: DragEvent) = {
-    val targetId = (de.target.asInstanceOf[HTMLElement]).id
-    de.dataTransfer.setData("Text", targetId)
-  }
+    def dragFn(de: DragEvent) = {
+        val targetId = (de.target.asInstanceOf[HTMLElement]).id
+        de.dataTransfer.setData("Text", targetId)
+    }
 
-  def onDropFn(de: DragEvent, slane: String, dlane: String) = {
-    val id = de.dataTransfer.getData("Text")
-    (de.target.asInstanceOf[HTMLDivElement]).appendChild(SKanban.d.getElementById(id))
-    // import skanban.Conversion._
-    SKanbanStorage.saveToStorage(slane, dlane)
-    SKanbanStorage.logStorage
-    document.getElementById(dlane).innerHTML
-  }
+    def onDropFn(de: DragEvent, slane: String, dlane: String) = {
+        val id = de.dataTransfer.getData("Text")
+        (de.target.asInstanceOf[HTMLDivElement]).appendChild(SKanban.d.getElementById(id))
+        // import skanban.Conversion._
+        SKanbanStorage.saveToStorage(slane, dlane)
+        document.getElementById(dlane).innerHTML
+    }
 
-  def ondragover(de: DragEvent) = {
-    de.preventDefault()
-    val targetEle = (de.target.asInstanceOf[HTMLDivElement])
-    de.dataTransfer.setData("Text", targetEle.id)
-  }
+    def ondragover(de: DragEvent) = {
+        de.preventDefault()
+        val targetEle = (de.target.asInstanceOf[HTMLDivElement])
+        de.dataTransfer.setData("Text", targetEle.id)
+    }
 
 }
